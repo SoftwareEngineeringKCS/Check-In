16db637ee4f1185e6cd803eee6ed6b9578cc64b8
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-10-29 20:16:57 -0500

    Update check_in.php
    User Inputs were validated for Check-In user story.

--- check_in.php
+++ check_in.php
@@ -14,45 +14,66 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		// Print the results:
 		echo "<h1>Check-In Result</h1>";
 		echo "<h2>" . $_POST['type_appointment'] . "</h2>";
-		if (!$_POST['student_id1'] == "") {
-			echo "<p>ID: " . $_POST['student_id1'] . "</p>";
-
-		} else {
-			echo "<p>ID: " . $_POST['student_id2'] . "</p>";
-			echo "<p>Name: " . $_POST['last_name'] . ", " . $_POST['first_name'] . "</p>";
-			$query = sprintf("SELECT description FROM Reasons WHERE id = '%s'", $_POST['reason']);
-			$result = mysqli_query($conex, $query);
-			$row = mysqli_fetch_array($result);
-			echo "<p>Reason: " . $row[0] . "</p>";
-			$query = sprintf("SELECT CONCAT(last_name,', ',first_name) FROM Consultants WHERE id = '%s'", $_POST['consultant']);
-			$result = mysqli_query($conex, $query);
-			$row = mysqli_fetch_array($result);
-			echo "<p>Consultant: " . $row[0] . "</p>";
-			$query = sprintf("SELECT CONCAT(detail,' ',building_id,room) FROM Locations WHERE id = '%s'", $_POST['location']);
-			$result = mysqli_query($conex, $query);
-			$row = mysqli_fetch_array($result);
-			echo "<p>Location: " . $row[0] . "</p>";
-			echo "<p><h2 style='color: #6CBB3C'><i>Take a seat please.</i></h2></p>";
+		if ($_POST['type_appointment'] == 'By-Appointment') {
+			if ($_POST['student_id1'] == '' || $_POST['confirm_num'] == '') {
+				if ($_POST['student_id1'] == '') echo "<p class='error'>\"ID\" cannot be empty!</p>";
+				if ($_POST['confirm_num'] == '') echo "<p class='error'>\"Confirmation Code\" cannot be empty!</p>";
+				echo "<p><a href='check_in.php'>TRY AGAIN</a></p>";
+			} else {
+				echo "<p>Confirmation Code: " . $_POST['confirm_num'] . "</p>";
+				echo "<p>Student ID: " . $_POST['student_id1'] . "</p>";
+				#
+				echo "<p><h2 style='color: #6CBB3C'><i>Take a seat please.</i></h2></p>";
+			}
+		} else if ($_POST['type_appointment'] == 'Walk-In') {
+			if ($_POST['location'] == '' || $_POST['consultant'] == '' || $_POST['reason'] == '' || 
+				$_POST['student_id2'] == '' || $_POST['first_name'] == '' || $_POST['last_name'] == '' || 
+				$_POST['email'] == '') {
+				if ($_POST['reason'] == '') echo "<p class='error'>\"Reason\" cannot be empty!</p>";
+				if ($_POST['student_id2'] == '') echo "<p class='error'>\"ID\" cannot be empty!</p>";
+				if ($_POST['first_name'] == '') echo "<p class='error'>\"First Name\" cannot be empty!</p>";
+				if ($_POST['last_name'] == '') echo "<p class='error'>\"Last Name\" cannot be empty!</p>";
+				if ($_POST['email'] == '') echo "<p class='error'>\"E-mail\" cannot be empty!</p>";
+				if ($_POST['consultant'] == '') echo "<p class='error'>\"Consultant\" cannot be empty!</p>";
+				if ($_POST['location'] == '') echo "<p class='error'>\"Location\" cannot be empty!</p>";
+				echo "<p><a href='check_in.php'>TRY AGAIN</a></p>";
+			} else {
+				echo "<p>Student ID: " . $_POST['student_id2'] . "</p>";
+				echo "<p>Name: " . $_POST['last_name'] . ", " . $_POST['first_name'] . "</p>";
+				$query = sprintf("SELECT description FROM Reasons WHERE id = '%s'", $_POST['reason']);
+				$result = mysqli_query($conex, $query);
+				$row = mysqli_fetch_array($result);
+				echo "<p>Reason: " . $row[0] . "</p>";
+				$query = sprintf("SELECT CONCAT(last_name,', ',first_name) FROM Consultants WHERE id = '%s'", $_POST['consultant']);
+				$result = mysqli_query($conex, $query);
+				$row = mysqli_fetch_array($result);
+				echo "<p>Consultant: " . $row[0] . "</p>";
+				$query = sprintf("SELECT CONCAT(detail,' ',building_id,room) FROM Locations WHERE id = '%s'", $_POST['location']);
+				$result = mysqli_query($conex, $query);
+				$row = mysqli_fetch_array($result);
+				echo "<p>Location: " . $row[0] . "</p>";
+				echo "<p><h2 style='color: #6CBB3C'><i>Take a seat please.</i></h2></p>";
+				#
+				mysqli_free_result($result);
+				mysqli_close($conex);
+			}
 		}
-
-		mysqli_free_result($result);
-		mysqli_close($conex);
 	} else { // Invalid submitted values.
 		echo '<h1>Error!</h1>
 		<p class="error">Please enter valid data.</p>';
 	}
 	
-} // End of main submission IF.
+}
 
-// Leave the PHP section and create the HTML form:
 ?>
 
 <div id="check_in_process"<?php if (isset($_POST['type_appointment'])) echo ' style="display: none;"'; ?>>
 	<h1>Check-In Process</h1>
 	<form action="check_in.php" method="post">	
+		<h2>All fields are required!</h2>
 		<p>Type of Check-In:<br><br>
 			<span class="input">
-				<input type="radio" name="type_appointment" value="By-Appoitnment"<?php if (isset($_POST['type_appointment']) && ($_POST['type_appointment'] == 'By-Appoitnment')) echo ' checked="checked"'; ?> onclick="byAppointment()" /> By-Appointment
+				<input type="radio" name="type_appointment" value="By-Appointment"<?php if (isset($_POST['type_appointment']) && ($_POST['type_appointment'] == 'By-Appoitnment')) echo ' checked="checked"'; ?> onclick="byAppointment()" /> By-Appointment
 				<input type="radio" name="type_appointment" value="Walk-In"<?php if (isset($_POST['type_appointment']) && ($_POST['type_appointment'] == 'Walk-In')) echo ' checked="checked"'; ?> onclick="walkIn()" /> Walk-In
 			</span>
 		</p>
@@ -92,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				if ($result) {
 					if (mysqli_num_rows($result) > 0) {
 						echo "<p>Location:";
-						echo "<br><select name='location' required='required' style='width: 200px'>";
+						echo "<br><select name='location' style='width: 200px'>";
 							while ($row = mysqli_fetch_array($result)) {
 								$loc_id = $row['id'];
 								$loc_location = $row['location'];
@@ -102,7 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 						echo "</p>";
 					} else {
 						echo "<p>Location:";
-						echo "<br><select name='location' required='required' style='width: 200px'>";
+						echo "<br><select name='location' style='width: 200px'>";
 							echo "<option value='' selected>EMPTY LIST</option>\n";
 						echo "</select>";
 						echo "</p>";
@@ -117,7 +138,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				if ($result) {
 					if (mysqli_num_rows($result) > 0) {
 						echo "<p>Consultant:";
-						echo "<br><select name='consultant' required='required' style='width: 200px'>";
+						echo "<br><select name='consultant' style='width: 200px'>";
 							while ($row = mysqli_fetch_array($result)) {
 								$con_id = $row['id'];
 								$con_consultant = $row['consultant'];
@@ -127,7 +148,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 						echo "</p>";
 					} else {
 						echo "<p>Consultant:";
-						echo "<br><select name='consultant' required='required' style='width: 200px'>";
+						echo "<br><select name='consultant' style='width: 200px'>";
 							echo "<option value='' selected>EMPTY LIST</option>\n";
 						echo "</select>";
 						echo "</p>";
@@ -142,7 +163,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				if ($result) {
 					if (mysqli_num_rows($result) > 0) {
 						echo "<p>Reason:";
-						echo "<br><select name='reason' required='required' style='width: 200px'>";
+						echo "<br><select name='reason' style='width: 200px'>";
 							while ($row = mysqli_fetch_array($result)) {
 								$re_id = $row['id'];
 								$re_description = $row['description'];
@@ -152,7 +173,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 						echo "</p>";
 					} else {
 						echo "<p>Reasons:";
-						echo "<br><select name='reason' required='required' style='width: 200px'>";
+						echo "<br><select name='reason' style='width: 200px'>";
 							echo "<option value='' selected>EMPTY LIST</option>\n";
 						echo "</select>";
 						echo "</p>";
@@ -166,16 +187,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			?>
 
 			<p>Student ID (without leading zeros):
-				<br><input type="text" name="student_id2" required="required" value="<?php if (isset($_POST['student_id2'])) echo $_POST['student_id2']; ?>" style="width: 190px" />
+				<br><input type="text" name="student_id2" value="<?php if (isset($_POST['student_id2'])) echo $_POST['student_id2']; ?>" style="width: 190px" />
 			</p>
 			<p>First Name:
-				<br><input type="text" name="first_name" required="required" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>" style="width: 190px" />
+				<br><input type="text" name="first_name" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>" style="width: 190px" />
 			</p>
 			<p>Last Name:
-				<br><input type="text" name="last_name" required="required" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>" style="width: 190px" />
+				<br><input type="text" name="last_name" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>" style="width: 190px" />
 			</p>
 			<p>E-mail:
-				<br><input type="text" name="email" required="required" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" style="width: 190px" />
+				<br><input type="text" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" style="width: 190px" />
 			</p>
 		</div>
 		<div id="show_submit" style="display: none;">
