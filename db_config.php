614a4e982d7b2a122c3a63c91e9afe514d3b7275
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-11-02 18:15:05 -0500

    Update db_config.php
    Conex added.

--- db_config.php
+++ db_config.php
@@ -3,4 +3,6 @@
 	$login = "seds2017";
 	$password = "2017fall";
 	$database = "seds2017db";
+
+	$conex = mysqli_connect($server, $login, $password, $database) or die("Error: Unable to connect to DataBase.");	
 ?>
