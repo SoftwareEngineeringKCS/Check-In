905b7d26b6b3da101cf1146ea8516dba1d6dae4d
Jorge Espada <32748368+espadajo@users.noreply.github.com>
2017-11-15 19:49:28 -0500

    Update confirm_walk_in.php

--- confirm_walk_in.php
+++ confirm_walk_in.php
@@ -1,30 +1,65 @@
 <?php #JORGE ESPADA
 	
+	function createCode($studentid, $btndatetime) {
+		include ('includes/db_config_function.php');
+		$code = "";
+		$c = 1;	
+		while ($c > 0) {
+			$random_hash = md5(uniqid(rand(), true));
+			$processed_hash = strtoupper("K" . substr($random_hash, 3,3) . substr($random_hash, 9, 3) . substr($random_hash, 15, 3));
+			$f_query = sprintf("SELECT confirm_num FROM Students_Appointment WHERE confirm_num = '%s'", $processed_hash);
+			$f_result = mysqli_query($f_conex, $f_query);
+			if ($f_result) {
+				if (mysqli_num_rows($f_result) > 0) {
+					$c = 1;
+				} else {
+					$code = $processed_hash;
+					$c = 0;
+				}
+				mysqli_free_result($f_result);
+			} else {
+				echo "<p class='error'>Problem trying to create dynamic confirmation code, a static code was created instead.</p>";
+				# Manual Code.
+				$code = date_format(date_create($btndatetime), "YmdHi") . "-" . $studentid;
+				$c = 0;
+			}
+		}
+		mysqli_close($f_conex);
+		return $code;
+	}
+	
 	# If ID but no Email.
 	include ('includes/db_config.php');
 
-	date_default_timezone_set('America/New_York');
-
 	// Print the results:
-	echo "<h1>Check-In Result</h1>";
-	echo "<h2>Walk-In</h2>";
+	echo "<h1>Appointment Result</h1>";
 
 	$getSID = $_REQUEST['getSID'];
 	$getSFN = $_REQUEST['getSFN'];
 	$getSLN = $_REQUEST['getSLN'];
 	$getSEM = $_REQUEST['getSEM'];
+	$getSCP = $_REQUEST['getSCP'];
+	$getBTN = $_REQUEST['getBTN'];
 	$getCID = $_REQUEST['getCID'];
 	$getLID = $_REQUEST['getLID'];
-	$getTA = $_REQUEST['getTA'];
 	$getRID = $_REQUEST['getRID'];
 
 	# Update Student.
-	$query = sprintf("UPDATE Students SET first_name = '%s', last_name = '%s', email = '%s' WHERE id = '%s'", $getSFN, $getSLN, $getSEM, $getSID);
+	$query = sprintf("UPDATE Students SET first_name = '%s', last_name = '%s', email = '%s', cell_phone = '%s' WHERE id = '%s'", $getSFN, $getSLN, $getSEM, $getSCP, $getSID);
 	$result = mysqli_query($conex, $query);
-	# Save check-in.
-	$set_in_datetime = date("Y-m-d H:i:s");
-	$query = sprintf("INSERT INTO Students_Check_In VALUES(NULL, 1, '%s', '%s', '%s', '%s', '%s', '%s', '')", $getSID, $getCID, $getLID, $getTA, $getRID, $set_in_datetime);
+	# Save appointment.
+	$pos = strpos($getBTN, ",");
+	$getTimeId = substr($getBTN, 0, $pos);
+	$getDateTime = substr($getBTN, $pos+1);
+	$getCode = createCode($getSID, $getDateTime);
+	$query = sprintf("INSERT INTO Students_Appointment VALUES(NULL, '%s', '%s', '%s', '%s', '%s', '%s', 0, 0, '', 0, '')", $getSID, $getCID, $getLID, $getRID, $getDateTime, $getCode);
 	$result = mysqli_query($conex, $query);
+	# Update Time Status.
+	$query = sprintf("UPDATE Availability_Times SET free = 0 WHERE id = '%s'", $getTimeId);
+	$result = mysqli_query($conex, $query);
+	# Send confirmation code (bye email).
+
+
 	# Show results.
 	echo "<p>Student ID: " . $getSID . "</p>";
 	echo "<p>Name: " . $getSLN . ", " . $getSFN . "</p>";
@@ -52,7 +87,7 @@
 	} else {
 		echo "<p class='error'>Location: Error!</p>";
 	}
-	echo "<p><h2 style='color: #6CBB3C'>Take a seat please.</h2></p>";
+	echo "<p><h2 style='color: #6CBB3C'>A Confirmation Code was sent to your email!</h2></p>";
 
 	mysqli_free_result($result);
 	mysqli_close($conex);
